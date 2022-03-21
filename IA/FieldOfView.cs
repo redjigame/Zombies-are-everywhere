06807c77d4df2d4f905891a56b1de817b6093b25
Renaud redjigame <48740296+redjigame@users.noreply.github.com>
2022-03-20 22:26:44 -0300

    Add files via upload

---
+++ IA/FieldOfView.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    public Transform partToRotate;
+    public bool hasDetected;
+
+    public float viewRadius;
+    [Range(0, 360)]
+    public float viewAngle;
+
+    public LayerMask targetMask;
+    public LayerMask obstacleMask;
+    [HideInInspector]
+    public List<Transform> visibleTargets = new List<Transform>();
+
+    void Start()
+    {
+        hasDetected = false;
+        StartCoroutine("FindTargetsWithDelay", 0.2f);
+
+    }//Start
+
+    IEnumerator FindTargetsWithDelay(float delay)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            FindVisibleTargets();
+        }
+    }//FindTargetsWithDelay
+
+    void FindVisibleTargets()
+    {
+        visibleTargets.Clear();
+        Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+
+        for (int i = 0; i < targetsInViewRadius.Length; i++)
+        {
+            Transform target = targetsInViewRadius[i].transform;
+            Vector3 dirToTarget = (target.position - transform.position).normalized;
+            if (Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2)
+            {
+                float distToTarget = Vector3.Distance(transform.position, target.position);
+                if (!Physics.Raycast(transform.position, dirToTarget, distToTarget, obstacleMask))
+                {
+                    visibleTargets.Add(target);
+
+                    hasDetected = true;
+                }
+            }
+        }//for
+    }//FindVisibleTarget
+
+    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDegrees += transform.eulerAngles.y;
+        }
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }//DirFromAngle
+
+}//Class
---
+++ IA/IaFootstep.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IaFootstep : MonoBehaviour
+{
+    AudioSource audioSource;
+    public Transform leftFoot;
+    public Transform rightFoot;
+
+    [SerializeField] List<AudioClip> footStepsGrassSound = new List<AudioClip>();
+    [SerializeField] List<AudioClip> footStepsSnowSound = new List<AudioClip>();
+    [SerializeField] List<AudioClip> footStepsConcreteSound = new List<AudioClip>();
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void CharacterFootstepSound(int footIndex)
+    {
+        if (footIndex == 0)
+        {
+            RaycastTerrain(leftFoot.position);
+        }
+        else if (footIndex == 1)
+        {
+            RaycastTerrain(rightFoot.position);
+        }
+
+    }//PlayerFootstepSound
+
+    private void RaycastTerrain(Vector3 position)
+    {
+        LayerMask layerMask = LayerMask.GetMask("Terrain");
+        Ray ray = new Ray(position, Vector3.down);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, layerMask))
+        {
+            string hitTag = hit.collider.gameObject.tag;
+            if (hitTag == "Grass")
+            {
+                PlayRandomSound(footStepsGrassSound);
+            }
+            else if (hitTag == "Smow")
+            {
+                PlayRandomSound(footStepsSnowSound);
+
+            }
+            else if (hitTag == "Concrete")
+            {
+                PlayRandomSound(footStepsConcreteSound);
+            }
+        }
+    }//RaycastTerrain
+
+    void PlayRandomSound(List<AudioClip> audioClips)
+    {
+        if (audioClips.Count > 0 && audioSource)
+        {
+            int randomNum = UnityEngine.Random.Range(0, audioClips.Count - 1);
+            audioSource.PlayOneShot(audioClips[randomNum]);
+        }
+    }//PlayRandomSound
+}//Class
